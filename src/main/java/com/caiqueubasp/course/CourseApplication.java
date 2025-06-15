a93a01feeb46104fd609dcf11b96b2ebb0d62428
Caique Souza <caiqueubasp@gmail.com>
2025-06-15 12:44:13 -0300

    start config database

--- src/main/java/com/caiqueubasp/course/CourseApplication.java
+++ src/main/java/com/caiqueubasp/course/CourseApplication.java
@@ -3,7 +3,8 @@ package com.caiqueubasp.course;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.caiqueubasp.course", "com.caiqueubasp.resources"})
+@SpringBootApplication(scanBasePackages = {"com.caiqueubasp.course", "com.caiqueubasp.resources", 
+"com.caiqueubasp.entities"})
 public class CourseApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/caiqueubasp/entities/User.java
+++ src/main/java/com/caiqueubasp/entities/User.java
@@ -1,9 +1,19 @@
 package com.caiqueubasp.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User implements java.io.Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
@@ -57,7 +67,6 @@ public class User implements java.io.Serializable {
                 + "id=" + id
                 + ", name='" + name + '\''
                 + ", email='" + email + '\''
-                + ", password='" + password + '\''
                 + '}';
     }
 
