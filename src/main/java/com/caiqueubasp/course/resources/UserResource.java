679a08b83d3b86bb5ade5acd860bac95f27472ca
Caique Souza <caiqueubasp@gmail.com>
2025-06-21 13:11:25 -0300

    feat: implementa PUT user

--- src/main/java/com/caiqueubasp/course/resources/UserResource.java
+++ src/main/java/com/caiqueubasp/course/resources/UserResource.java
@@ -16,6 +16,8 @@ import com.caiqueubasp.course.services.UserService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 
@@ -52,6 +54,12 @@ public class UserResource {
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         userService.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+        obj = userService.update(id, obj);
+        return ResponseEntity.ok().body(obj);
     }
     
     
