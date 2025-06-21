0e049a21eec60316eea9bf6c702f0a884127ba66
Caique Souza <caiqueubasp@gmail.com>
2025-06-21 13:32:03 -0300

    feat: implementa ExceptionHandler

--- src/main/java/com/caiqueubasp/course/services/UserService.java
+++ src/main/java/com/caiqueubasp/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.caiqueubasp.course.entities.User;
 import com.caiqueubasp.course.repositories.UserRepository;
+import com.caiqueubasp.course.services.excepctions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = userRepository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
@@ -43,7 +44,7 @@ public class UserService {
         entity.setEmail(obj.getEmail());
         entity.setPhone(obj.getPhone());
     }
-    
+
     public User getReferenceById(Long id) {
         return userRepository.getReferenceById(id);
     }
